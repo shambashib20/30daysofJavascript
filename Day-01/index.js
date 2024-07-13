3e4aeea4b511da201a72bce82f7874f529d4f459
shambashib20 <shambashibmajumdar2000@gmail.com>
2024-07-13 19:46:14 +0530

    feat: added Day - 01 challenges

---
+++ Day-01/index.js
@@ -0,0 +1,50 @@
+// Solution to Activity 1 Variable Declaration
+
+
+var num = 1;
+console.log(num);
+
+let string = "Hello 30 days Javascript!";
+console.log(string);
+
+
+
+// Solution to Activity 2 Constant Declaration
+
+const booleanValue = true;
+
+console.log(booleanValue);
+
+// Solution to Activity 3 Data Types 
+let numericValue=88;
+let stringValue="I am Developer";
+let boolValue=true;
+let objValue={
+    name:"Lamine Yamal",
+    profile:"Footballer"
+}
+let arrValue = ["Developer","Freelancer"];
+
+console.log(typeof numericValue);
+console.log(typeof stringValue);
+console.log(typeof boolValue);
+console.log(typeof objValue);
+console.log(typeof arrValue);
+
+
+
+// Solution to Activity 4 
+
+let assignCheckValue="Software Developer"
+console.log(assignCheckValue);
+
+assignCheckValue="React Developer";
+console.log(assignCheckValue);
+
+//Solution to Activity 5 Understanding Const
+
+const understandingConstValue=true;
+console.log(understandingConstValue);
+
+understandingConstValue=false;
+console.log(understandingConstValue);
